fb3380da72b5728ba3f7c6f1ab4138b9f5d6660e
Scott Berger <contact@scottberger.xyz>
2016-05-16 21:24:33 +0000

    Removed source git

--- source/spec/models/url_spec.rb
+++ source/spec/models/url_spec.rb
@@ -12,15 +12,9 @@ RSpec.describe Url, :type => :model do
   end
   
   it "is invalid with a duplicate short" do
-    Url.create(
-      name: 'test0.com', count: 0,
-      short: 'abc123'
-    )
-    url = Url.new(
-      name: 'test.com', count: 0,
-      short: 'abc123'
-    )
+    FactoryGirl.create(:url, short: 'abc123')
+    url = FactoryGirl.build(:url, short: 'abc123')
     url.valid?
-    expect(url.errors[:short]).to include("has already been taken")
+    expect(url.errors[:short]).to include('has already been taken')
   end
 end
